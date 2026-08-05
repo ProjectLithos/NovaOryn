8ee822ab0bff65db11833262abfda935bcab80c6
projectlithos <projectlithos@gmail.com>
2026-08-05 13:39:18 +0100

    Update NovaOryn to 0.0.53

--- src/NovaOryn.Core/KernelAttributes.cs
+++ src/NovaOryn.Core/KernelAttributes.cs
@@ -1,12 +1,23 @@
 namespace NovaOryn.Core;
 
-[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
-public sealed class KernelEntryAttribute : Attribute;
+/// <summary>Marks the managed method that serves as the kernel entry point.</summary>
+[AttributeUsage(AttributeTargets.Method, Inherited = false)]
+public sealed class KernelEntryAttribute : Attribute
+{
+}
 
-[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
-public sealed class DoesNotReturnAttribute : Attribute;
+/// <summary>Marks a method that does not return to its caller.</summary>
+[AttributeUsage(AttributeTargets.Method, Inherited = false)]
+public sealed class DoesNotReturnAttribute : Attribute
+{
+}
 
+/// <summary>Represents a NovaOryn semantic version.</summary>
+/// <param name="Major">The major version component.</param>
+/// <param name="Minor">The minor version component.</param>
+/// <param name="Patch">The patch version component.</param>
 public readonly record struct VersionInfo(ushort Major, ushort Minor, ushort Patch)
 {
-    public static VersionInfo Current => new(0, 0, 16);
+    /// <summary>Gets the version of the currently installed NovaOryn SDK.</summary>
+    public static VersionInfo Current => new(0, 0, 53);
 }
